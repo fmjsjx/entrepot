2d6f3fe8c7c4f33eeec5057672a4707ca99b9927
fmjsjx <fmjsjx@163.com>
2022-02-08 17:27:36 +0800

    Fix with spring-boot

--- entrepot-core/build.gradle.kts
+++ entrepot-core/build.gradle.kts
@@ -1,22 +1,23 @@
 plugins {
     id("entrepot.parent-conventions")
+    id("org.springframework.boot") version "2.6.3" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 description = "Entrepot Core"
 
+dependencyManagement {
+    imports {
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+    }
+}
+
 dependencies {
-    // spring-boot-dependencies
-    api(platform("org.springframework.boot:spring-boot-dependencies:2.6.3"))
-    annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:2.6.3"))
-    
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     implementation("org.slf4j:slf4j-api")
     implementation("com.github.fmjsjx:libcommon-util")
     api("io.netty:netty-buffer")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation("org.apache.logging.log4j:log4j-slf4j-impl")
-
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- entrepot-server/build.gradle.kts
+++ entrepot-server/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
-    java
-    id("org.springframework.boot") version "2.6.3"
     id("entrepot.parent-conventions")
+    id("org.springframework.boot") version "2.6.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    java
     distribution
 }
-
-apply(plugin = "io.spring.dependency-management")
 
 description = "Entrepot Server"
 
